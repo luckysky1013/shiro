ed56e42045b91bafdfac4c1c52280a868508ceaf
刘健 <luckysky1013@163.com>
2019-04-19 14:01:07 +0800

    shiro demo

---
+++ shiro-study/shiro-realm/src/main/java/com/emma/shiro/realm/MyRealm1.java
@@ -0,0 +1,38 @@
+package com.emma.shiro.realm;
+
+import org.apache.shiro.authc.*;
+import org.apache.shiro.realm.Realm;
+
+/**
+ * @author liujian
+ * @date 2019/4/19
+ */
+public class MyRealm1 implements Realm{
+	@Override
+	public String getName() {
+		return "myRealm1";
+	}
+
+	@Override
+	public boolean supports(AuthenticationToken authenticationToken) {
+		return authenticationToken instanceof UsernamePasswordToken;
+	}
+
+	@Override
+	public AuthenticationInfo getAuthenticationInfo(AuthenticationToken authenticationToken)
+			throws AuthenticationException {
+		//获取用户名
+		String username=(String) authenticationToken.getPrincipal();
+		//获取密码
+		String password=new String ((char[])authenticationToken.getCredentials() );
+		if(!"zhang".equals(username)){
+			//用户名错误
+			throw new UnknownAccountException();
+		}
+		if(!"123".equals(password)){
+			//密码错误
+			throw new IncorrectCredentialsException();
+		}
+		return new SimpleAuthenticationInfo(username,password,getName());
+	}
+}
